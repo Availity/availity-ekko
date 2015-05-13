52b317da35f8a50d076e6663cec1ffd3b72ac45c
unknown <Kpowers@HR44SY1.availity.net>
2015-05-13 15:31:52 -0400

    added proxy value for testing in config file, and added proxy code to middleware/testing

--- config.js
+++ config.js
@@ -50,6 +50,7 @@ var config = {
       api: {
         host: "127.0.0.1",
         port: 7777,
+        proxy: true,
         proxies: [
           {
             context: "/api",
--- server/middleware/testing.js
+++ server/middleware/testing.js
@@ -2,6 +2,7 @@ var expressLogger = require('morgan');
 var errorhandler = require('errorhandler');
 var compression = require('compression');
 var methodOverride = require('method-override');
+var cors = require('cors');
 var bodyParser = require('body-parser');
 var busboy = require('connect-busboy');
 
@@ -14,8 +15,19 @@ module.exports = function production() {
   config.app.use(expressLogger('dev'));
   config.app.use(errorhandler());
   config.app.use(compression());
-  config.app.use(methodOverride('X-HTTP-Method-Override'));
+  config.app.use(cors());
   config.app.use(negotiate());
+
+  // Proxies must be configured before the mock routes so they can be intercepted
+  // and forwarded to appropriate server
+  if(config.isProxyEnabled()) {
+    logger.success('proxy configurations detected');
+    config.app.use(proxy());
+  } else {
+    logger.warn('no proxy configurations detected');
+  }
+
+  config.app.use(methodOverride('X-HTTP-Method-Override'));
 
   config.app.use(bodyParser.json()); // parse application/json
   config.app.use(bodyParser.urlencoded({
