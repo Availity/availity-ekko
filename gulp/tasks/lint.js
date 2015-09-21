61589da8449e5be7d7d9fa999b089c55bfdc7228
Rob McGuinness <rob.mcguinness@availity.com>
2015-09-20 21:37:41 -0400

    convert to eslint

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -1,17 +1,14 @@
 var gulp = require('gulp');
+var eslint = require('gulp-eslint');
 
 gulp.task('lint', ['lint:js']);
 
 gulp.task('lint:js', function () {
 
-  var jshint = require('gulp-jshint');
-  var stylish = require('jshint-stylish');
-  var jscs = require('gulp-jscs');
-
   var config = require('../config');
 
   gulp.src(config.js.src)
-    .pipe(jscs())
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
+    .pipe(eslint(context.settings.js.linter))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
